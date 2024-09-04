74effbf76401181d82234058ab9b27ff87a1e7ff
Anton Reinhard <anton.reinhard@proton.me>
2024-09-04 15:03:43 +0200

    Remove files

--- testgpu/runtests.jl
+++
@@ -1,7 +0,0 @@
-using SafeTestsets
-using CUDA
-
-@safetestset "Utility Unit Tests                  " begin
-    include("unit_tests_utility.jl")
-end
-# TODO: Make a new simple test model and rewrite tests here
