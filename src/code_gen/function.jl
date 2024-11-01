f0dbbc7e3085f2eb02764dc4558215dde6ed388f
AntonReinhard <anton.reinhard@proton.me>
2024-11-01 13:03:30 +0100

    Turn closures off by default

--- src/code_gen/function.jl
+++ src/code_gen/function.jl
@@ -17,10 +17,10 @@ in your top level.
 
 ## Keyword Arguments
 
-`closures_size` (default=500): The size of closures to use in the main generated code. This specifies the size of code blocks across which the compiler cannot optimize. For sufficiently large functions, a larger value means longer compile times but potentially faster execution time.
+`closures_size` (default=0 (off)): The size of closures to use in the main generated code. This specifies the size of code blocks across which the compiler cannot optimize. For sufficiently large functions, a larger value means longer compile times but potentially faster execution time.
 """
 function get_compute_function(
-    graph::DAG, instance, machine::Machine, context_module::Module; closures_size=500
+    graph::DAG, instance, machine::Machine, context_module::Module; closures_size=0
 )
     tape = gen_tape(graph, instance, machine, context_module)
 
