38d85d389f4f45660030952c85c00527f9e49c43
Anton Reinhard <anton.reinhard@proton.me>
2025-10-21 23:12:17 +0200

    Fibonacci example and manual (#69)
    * Add small fibonacci example
    
    * Add RGF.jl to docs dependencies for the literate example
    
    * Improve fibonacci example according to feedback
    
    * Add manual section
    
    * Mention Dagger.jl
    
    * Update the Readme
    
    * Add 2025 to License years
    
    * Add input_type to necessary functions to implement
    
    * Update docs/src/examples/fibonacci.jl
    
    Co-authored-by: Uwe Hernandez Acosta <szabo137@users.noreply.github.com>
    
    * Add a sentence for algorithm explanation
    
    Add a link to the generated ipynb notebook
    
    ---------
    
    Co-authored-by: Uwe Hernandez Acosta <szabo137@users.noreply.github.com>

--- docs/make.jl
+++ docs/make.jl
@@ -4,22 +4,43 @@ project_path = Base.Filesystem.joinpath(Base.Filesystem.dirname(Base.source_path
 Pkg.develop(; path = project_path)
 
 using Documenter
+using Literate
 using ComputableDAGs
+
+# setup examples using Literate.jl
+literate_paths = [
+    (
+        Base.Filesystem.joinpath(project_path, "docs/src/examples/fibonacci.jl"),
+        Base.Filesystem.joinpath(project_path, "docs/src/examples/"),
+    ),
+]
+
+for (file, output_dir) in literate_paths
+    Literate.markdown(file, output_dir; documenter = true)
+    Literate.notebook(file, output_dir)
+end
 
 pages = [
     "index.md",
     "Manual" => "manual.md",
+    "Examples" => [
+        "Fibonacci" => "examples/fibonacci.md",
+    ],
     "Library" => [
         "Public" => "lib/public.md",
         "Graph" => "lib/internals/graph.md",
         "Node" => "lib/internals/node.md",
         "Task" => "lib/internals/task.md",
+        "Properties" => "lib/internals/properties.md",
         "Operation" => "lib/internals/operation.md",
+        "Estimation" => "lib/internals/estimator.md",
+        "Optimization" => "lib/internals/optimization.md",
         "Models" => "lib/internals/models.md",
         "Diff" => "lib/internals/diff.md",
-        "Utility" => "lib/internals/utility.md",
+        "Scheduler" => "lib/internals/scheduler.md",
         "Code Generation" => "lib/internals/code_gen.md",
         "Devices" => "lib/internals/devices.md",
+        "Utility" => "lib/internals/utility.md",
     ],
     "Contribution" => "contribution.md",
 ]
