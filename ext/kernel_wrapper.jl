301c3a1116b0decc5f3514b5c2317519771444ac
AntonReinhard <anton.reinhard@proton.me>
2025-11-06 11:37:02 +0100

    Review suggestions

--- ext/kernel_wrapper.jl
+++ ext/kernel_wrapper.jl
@@ -1,35 +1,35 @@
 """
-    KAWrapper{T, UUID}
+    KAWrapper{T, ID}
 
-A wrapper around a KernelAbstractions kernel. Takes the `kernel::T` and a `UUID::Val`.
+A wrapper around a KernelAbstractions kernel. Takes the `kernel::T` and an `ID::Val`.
 
-This is necessary to insert the Val to the KernelAbstractions kernel without needing the user to do it manually.
+This is necessary to insert the id to the KernelAbstractions kernel without needing the user to do it manually.
 The Val itself is necessary to be able to define multiple different kernels working on the same input type. It is used in the expression cache as the key, and dispatched on in the `@generated` function.
 """
-struct KAWrapper{T, UUID}
+struct KAWrapper{T, ID}
     kernel::T
-    val::UUID
+    id::ID
 end
 
 """
-    KAWrapperKernel{T, UUID, Args, KWArgs}
+    KAWrapperKernel{T, ID, Args, KWArgs}
 
 The second level of wrapping, to imitate the way that KernelAbstractions kernels are called: `kernel(<kernel config/backend>)(<runtime arguments>)`.
 """
-struct KAWrapperKernel{T, UUID, Args, KWArgs}
+struct KAWrapperKernel{T, ID, Args, KWArgs}
     kernel::T
-    val::UUID
+    id::ID
     args::Args
     kwargs::KWArgs
 end
 
 # initial level, args and kwargs are the kernel config, stored in the KAWrapperKernel
-@inline function (k::KAWrapper{T, UUID})(args...; kwargs...) where {T, UUID}
-    return KAWrapperKernel(k.kernel, k.val, args, kwargs)
+@inline function (k::KAWrapper{T, ID})(args...; kwargs...) where {T, ID}
+    return KAWrapperKernel(k.kernel, k.id, args, kwargs)
 end
 
-# second level, wraps the actual call, inserting the kernel config args/kwargs, and calling with the runtime args + the stored val
-@inline function (k::KAWrapperKernel{T, UUID, Args})(args...; kwargs...) where {T, UUID, Args}
-    k.kernel(k.args...; k.kwargs...)(args..., k.val; kwargs...)
+# second level, wraps the actual call, inserting the kernel config args/kwargs, and calling with the runtime args + the stored id
+@inline function (k::KAWrapperKernel{T, ID, Args})(args...; kwargs...) where {T, ID, Args}
+    k.kernel(k.args...; k.kwargs...)(args..., k.id; kwargs...)
     return nothing
 end
